2a434014b33c33d6d45d0320c526c537b8531e36
Sarah Hart <sarah.hart.nc@gmail.com>
2022-04-10 17:20:20 -0400

    basic server setup

---
+++ server.js
@@ -0,0 +1,20 @@
+// Necessary items of interest :) 
+const express = require('express');
+const { appendFile } = require('fs');
+
+const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Set up the port to listen
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});

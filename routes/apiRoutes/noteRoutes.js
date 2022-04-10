2247a92b55e05780326fea783ea26cd318e1807f
Sarah Hart <sarah.hart.nc@gmail.com>
2022-04-10 18:47:22 -0400

    update routes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -11,10 +11,18 @@ router.get('/notes', (req, res) => {
 // Post function creates new id number
 router.post('/notes', (req, res) => {
     // req.body something to define title, text, id
+    
+    // saves new note/defines note
+    let note = createNewNote(notes, newNote)
     // puts note in JSON format
     res.json(note);
 });
 
+// Delete route
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(notes, req.params.id);
+    res.json(notes);
+})
 
 // exports module 
 module.exports = router;

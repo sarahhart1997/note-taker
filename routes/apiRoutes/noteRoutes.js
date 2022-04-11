e32320e307c60909e164202bafc614777c30b6ee
Sarah Hart <sarah.hart.nc@gmail.com>
2022-04-11 07:55:40 -0400

    finalize post route and notes are appearing

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,21 +1,38 @@
 // Core Setup
 const router = require('express').Router();
 const { createNewNote, deleteNote } = require('../../lib/notes');
-const { notes } = require('../../db/db.json');
+const notes = require('../../db/db.json');
+const { uuid, fromString } = require('uuidv4');
+const fs = require('fs');
 
 // reads db and returns all saved notes
 router.get('/notes', (req, res) => {
-    res.json(notes)
+    res.send(notes)
+    console.log(notes)
 });
 
 // Post function creates new id number
 router.post('/notes', (req, res) => {
-    // req.body something to define title, text, id
-    
-    // saves new note/defines note
-    let note = createNewNote(notes, newNote)
-    // puts note in JSON format
-    res.json(note);
+    // create unique id for each note
+    let noteId = uuid();
+    let completeNote = {
+        id: noteId, 
+        title: req.body.title,
+        text: req.body.text,
+    }
+    // 
+    fs.readFile('./db/db.json', 'utf-8',(error, data) => {
+        if (error) throw (error);
+        // turn JSON into an object
+        let currentNotes = JSON.parse(data);
+        currentNotes.push(completeNote);
+        // turn object back into JSON
+        fs.writeFile('./db/db.json', JSON.stringify(currentNotes), (error) => {
+            if (error) throw (error);
+            res.send(currentNotes);
+            console.log('Note created successfully')
+        })
+    })
 });
 
 // Delete route
